f1e790a362e473dfadd4ab8263eb53ba42abb269
anewardeberg <anewardeberg@gmail.com>
2021-11-03 21:46:07 +0100

    refractored

--- Trail 3.0/Controllers/ContactListViewController.swift
+++ Trail 3.0/Controllers/ContactListViewController.swift
@@ -7,25 +7,25 @@
 
 import UIKit
 
-class ContactListViewController: UITableViewController {
+class ContactListViewController: UITableViewController, UITabBarDelegate {
      
      
      // MARK: Egen funksjon som lagrer bilder
      
-     private var contactViewModels = [ContactCellViewModel]()
-     var selectedContact: ContactCellViewModel?
+     private var contactModels = [ContactModel]()
+     var selectedContact: ContactModel?
      
      override func viewDidLoad() {
           super.viewDidLoad()
           
           // Do any additional setup after loading the view.
-      
+          
           API.shared.getRandomContacts{ [weak self] result in
                switch result {
                case .success(let contacts):
-                    self?.contactViewModels = contacts.compactMap({
+                    self?.contactModels = contacts.compactMap({
                          
-                         ContactCellViewModel(
+                         ContactModel(
                               firstName: $0.name.first,
                               lastName: $0.name.last,
                               age: $0.dob.age,
@@ -33,6 +33,8 @@ class ContactListViewController: UITableViewController {
                               city: $0.location.city,
                               state: $0.location.state,
                               postcode: $0.location.postcode,
+                              latitude: $0.location.coordinates.latitude,
+                              longitude: $0.location.coordinates.latitude,
                               cell: $0.cell,
                               email: $0.email,
                               imgMedium: $0.picture.medium,
@@ -40,7 +42,7 @@ class ContactListViewController: UITableViewController {
                               imgThumb: $0.picture.thumbnail
                               
                          )})
-               
+                    
                     DispatchQueue.main.async {
                          self?.tableView.reloadData()
                     }
@@ -48,11 +50,35 @@ class ContactListViewController: UITableViewController {
                     print(error)
                }
           }
+          
+          
+     }
+     
+     //TODO: Fix this.
+     private func tabBar(_ tabBar: UITabBar, didSelect navigationController: UINavigationController) {
+          if navigationController.restorationIdentifier == "contactListNC"{
+               print("==== CONTACT LIST VIEW CONTROLLER")
+          } else if navigationController.restorationIdentifier == "mapNC" {
+               print("==== MAP VIEW CONTROLLER")
+          }
      }
      
      
+     //     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+     //          if viewController.isKind(of: MapViewController.self as AnyClass) {
+     //               let viewController = tabBarController.viewControllers?[1] as! MapViewController
+     //               viewController.contacts = self.contactModels
+     //          }
+     //          return true
+     //     }
+     //
+     
+     //     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+     //          <#code#>
+     //     }
+     
      override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-          return contactViewModels.count
+          return contactModels.count
      }
      
      override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -61,15 +87,16 @@ class ContactListViewController: UITableViewController {
                withIdentifier: "ContactCell",
                for: indexPath
           )
-          cell.imageView?.loadImage(urlString: contactViewModels[indexPath.row].imgMedium)
-          cell.textLabel?.text = "\(contactViewModels[indexPath.row].firstName) \(contactViewModels[indexPath.row].lastName)"
-
+          cell.imageView?.loadImage(urlString: contactModels[indexPath.row].imgMedium)
+          cell.textLabel?.text = "\(contactModels[indexPath.row].firstName) \(contactModels[indexPath.row].lastName)"
+          
           return cell
      }
      
      override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-               selectedContact = contactViewModels[indexPath.row]
-
+          selectedContact = contactModels[indexPath.row]
+          
+          // TODO: Format dates
           print("====== BIRTHDAY")
           print(selectedContact?.date)
           
@@ -77,8 +104,9 @@ class ContactListViewController: UITableViewController {
           
           
           
-     
+          
      }
+     
      
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
           if segue.identifier == "goToContactDetail" {
@@ -87,6 +115,11 @@ class ContactListViewController: UITableViewController {
                     destinationVC.contact = selectedContact
                }
           }
+          
+          //         if segue.identifier == "goToMapView" {
+          //             let destinationVC = segue.destination as! UINavigationController
+          //             let targetController = destinationNC.navigationController?.topViewController
+          //         }
      }
      
      

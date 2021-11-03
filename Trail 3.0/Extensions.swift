a6d1388df4fb0ec6c821f789fe4900b59b33845d
anewardeberg <anewardeberg@gmail.com>
2021-11-03 09:43:42 +0100

    Added extension file

---
+++ Trail 3.0/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  Trail 3.0
+//
+//  Created by Ane Wardeberg on 02/11/2021.
+//
+
+import Foundation
+import UIKit
+
+
+// https://www.youtube.com/watch?v=OTcQnf6ziew
+
+//extension UIImageView {
+//    func load(url: String) {
+//        DispatchQueue.global().async { [weak, self] in
+//            if let data  = try  Data(contentsOf: url) {
+//
+//            }
+//        }
+//    }
+//}

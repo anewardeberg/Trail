343b39278b62ae27e57fac19cc71c4d1194cfbeb
anewardeberg <anewardeberg@gmail.com>
2021-11-12 20:16:59 +0100

    added data objects in ContactStorage

--- Trail 3.0/Models/ContactStorage.swift
+++ Trail 3.0/Models/ContactStorage.swift
@@ -9,5 +9,20 @@ import Foundation
 import CoreData
 
 public class ContactStore: NSManagedObject {
+    @NSManaged var firstName: String
+    @NSManaged var lastName: String
+    @NSManaged var age: Int
+    @NSManaged var date: String
+    @NSManaged var city: String
+    @NSManaged var state: String
+    @NSManaged var postcode: String
+    @NSManaged var latitude: String
+    @NSManaged var longitude: String
+    @NSManaged var cell: String
+    @NSManaged var id: String
+    @NSManaged var email: String
+    @NSManaged var imgMedium: String
+    @NSManaged var imgLarge: String
+    @NSManaged var imgThumb: String
     
 }

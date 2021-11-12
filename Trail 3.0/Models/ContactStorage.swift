f69316f490ec6621368b12e7a558c30b86cf6987
anewardeberg <anewardeberg@gmail.com>
2021-11-12 19:40:05 +0100

    added ContactStorage class and entity

---
+++ Trail 3.0/Models/ContactStorage.swift
@@ -0,0 +1,13 @@
+//
+//  ContactStorage.swift
+//  Trail 3.0
+//
+//  Created by Ane Wardeberg on 12/11/2021.
+//
+
+import Foundation
+import CoreData
+
+public class ContactStore: NSManagedObject {
+    
+}

9a28a01edbbf5f621f9602d0e0b9c804167b661b
anewardeberg <anewardeberg@gmail.com>
2021-11-14 14:27:35 +0100

    Map annotations fetched from core data

--- Trail 3.0/Controllers/MapViewController.swift
+++ Trail 3.0/Controllers/MapViewController.swift
@@ -6,63 +6,49 @@
 //
 import UIKit
 import MapKit
+import CoreData
 
 class MapViewController: UIViewController, MKMapViewDelegate,  CLLocationManagerDelegate {
     
     var contactLatitude: String?
     var contactLongitude: String?
     var contactImageURL: String?
-
+    
+    var contactList = [ContactStorage]()
+    
     
     @IBOutlet weak var mapView: MKMapView!
     var contactModels = [ContactModel]()
     
     override func viewWillAppear(_ animated: Bool) {
-        
         mapView.delegate = self
         if (tabBarController?.selectedIndex == 1) {
-            API.shared.getRandomContacts{ [weak self] result in
-                 switch result {
-                 case .success(let contacts):
-                      self?.contactModels = contacts.compactMap({
-                           ContactModel(
-                                firstName: $0.name.first,
-                                lastName: $0.name.last,
-                                age: $0.dob.age,
-                                date: $0.dob.date,
-                                city: $0.location.city,
-                                state: $0.location.state,
-                                postcode: $0.location.postcode,
-                                latitude: $0.location.coordinates.latitude,
-                                longitude: $0.location.coordinates.longitude,
-                                cell: $0.cell,
-                                id: $0.id.value,
-                                email: $0.email,
-                                imgMedium: $0.picture.medium,
-                                imgLarge: $0.picture.large,
-                                imgThumb: $0.picture.thumbnail
-
-                           )})
-
-                 case .failure(let error):
-                      print(error)
-                 }
-                
-                var contactLocations: [ContactLocation] = []
-                for contactModel in self!.contactModels {
-                    var contactCoordinates = CLLocationCoordinate2D(latitude: contactModel.latitude.toDouble(), longitude: contactModel.longitude.toDouble())
-                      contactLocations.append(ContactLocation(name: contactModel.firstName, coordinate: contactCoordinates))
-
+            let fetchRequest = NSFetchRequest<ContactStorage>(entityName: "ContactStorage")
+            
+            ModelManager.sharedManager.persistentContainer.viewContext.perform {
+                do {
+                    let results = try fetchRequest.execute()
+                    self.contactList = results
+                    print("==== FETCHED CONTACT LIST FROM CORE DATA")
+                    var contactLocation: [ContactLocation] = []
+                    for contact in self.contactList {
+                        var contactCoordinate = CLLocationCoordinate2D(latitude: contact.latitude.toDouble(), longitude: contact.longitude.toDouble())
+                          contactLocation.append(ContactLocation(name: contact.firstName, coordinate: contactCoordinate))
+                        print("==== CONTACT LATITUDE")
+                        print(contact.latitude)
+                    }
+                    
+                    self.mapView.addAnnotations(contactLocation)
+                } catch {
+                    print(error)
+#warning("alert user")
                 }
-                self!.mapView.addAnnotations(contactLocations)
-                
             }
 
 
-    
+            
+                
         }
-        // Custom Map Annotation Pin
-        // https://stackoverflow.com/questions/38274115/ios-swift-mapkit-custom-annotation
         
         if let contactLatitude = contactLatitude {
             print(contactLatitude)
@@ -71,17 +57,14 @@ class MapViewController: UIViewController, MKMapViewDelegate,  CLLocationManager
             let region = MKCoordinateRegion( center: contactCoordinates, latitudinalMeters: CLLocationDistance(exactly: 1000)!, longitudinalMeters: CLLocationDistance(exactly: 1000)!)
             mapView.setRegion(region, animated: true)
             let contactLocation = ContactLocation(name: "User", coordinate: contactCoordinates)
-    //                mapView.setCenter(contactCoordinates, animated: true)
-                    mapView.addAnnotation(contactLocation)
-                    mapView.addAnnotations([contactLocation])
-            
+            mapView.addAnnotation(contactLocation)
         }
         
         
         
     }
     
-
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
